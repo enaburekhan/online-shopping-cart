e541a8847bfd8c5c0bddcd620b40f7454be22ae9
enaburekhan <ericenaburekhan2018@gmail.com>
2021-09-14 11:27:36 +0100

    Install package.json, express and mongoose

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+
+// view engine
+
+
+// database connection
+// const dbURI = 'mongodb+srv://shopping-cart:Eron1234@nodetuts.q7qx7.mongodb.net/shopping-cart';
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+//   .then((result) => app.listen(3000))
+//   .catch((err) => console.log(err));
+
+// routes
+ app.listen(3000);
+ app.get('/', (req, res) => {
+    res.send('hello world');
+ });
+// app.get('/smoothies', (req, res) => res.render('smoothies'));
