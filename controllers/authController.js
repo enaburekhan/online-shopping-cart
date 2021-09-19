8599534a17fc5ada777411710259c197958a1120
enaburekhan <ericenaburekhan2018@gmail.com>
2021-09-19 20:23:58 +0100

    Create jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -19,6 +20,13 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'net ninja secret', {
+    expiresIn: maxAge    
+  })    
+}
+
 module.exports.signup_get = (req, res) => {
   res.render('signup');    
 };
@@ -32,6 +40,8 @@ module.exports.signup_post = async (req, res) => {
   
   try {
     const user = await User.create({ email, password });
+    const token = createToken(user_id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
     res.status(201).json(user);
   }
   catch (err) {
